23604d0d76f730c98e7c059a3df9ccc71b175024
Syed Hasan <syedhasan@Syeds-MacBook-Pro.local>
2016-12-25 12:34:00 -0500

    update tests

--- tests/secondtest.js
+++ tests/secondtest.js
@@ -2,9 +2,9 @@ module.exports = {
   '{{this should contain a small title of what the step is doing}}': function (browser) {
 
   browser
-  	.url('http://www.yahoo.com')
+  	.url('https://www.yahoo.com')
   	.waitForElementVisible('.IconNavSearch' , 2000)
-  	.setValue('input[name="p"]' , 'economy')
+  	.setValue('input[name="p"]' , 'knicks')
     .click('#uh-search-button')
     .waitForElementVisible('.reg.searchLeftTop' , 2000)
   }
